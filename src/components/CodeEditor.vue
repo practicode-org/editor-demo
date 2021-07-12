d88999eb8ed843f569e53a038afbcd4acdae07fe
Alexander Garustovich <yukigaru@gmail.com>
2021-07-12 09:07:22 +0300

    Add simple states and state label

--- src/components/CodeEditor.vue
+++ src/components/CodeEditor.vue
@@ -14,6 +14,7 @@
     </b-tabs>
 
     <div id="button-bar">
+      <div id="current-state">{{ stateText }}</div>
       <b-button id="run-btn" variant="success" v-on:click="onRun">Run</b-button>
       <b-dropdown id="menu-btn" no-caret text="..." toggle-text="" class="m-2">
         <b-dropdown-item href="#" v-on:click="onResetCode">Reset code</b-dropdown-item>
@@ -41,13 +42,24 @@ export default {
   data() {
     return {
       tabIndex: 0,
-      originalCode: '#include <iostream>\n#include<chrono>\n#include <thread>\n\nint main() {\n  for (int i = 0; i < 5; i++) {\n  std::this_thread::sleep_for(std::chrono::milliseconds(500));\nstd::cout << "Hello, guys!" << std::endl;\n}\n  return 0;\n}',
+      originalCode: '#include <iostream>\n#include <chrono>\n#include <thread>\n\nint main() {\n  for (int i = 0; i < 5; i++) {\n    std::this_thread::sleep_for(std::chrono::milliseconds(500));\n    std::cout << "Hello, guys!" << std::endl;\n  }\n  return 0;\n}',
       code: '',
       language: 'cpp',
+      state: 'idle'
     }
   },
   created: function() {
     this.onResetCode();
+  },
+  computed: {
+    stateText: function() {
+      if (this.state === 'compile') {
+        return 'Compiling...';
+      } else if (this.state === 'run') {
+        return 'Running...';
+      }
+      return '';
+    }
   },
   methods: {
     onResetCode() {
@@ -63,6 +75,10 @@ export default {
     },
 
     onRun() {
+      if (this.state !== 'idle') {
+        return;
+      }
+
       const code = this.code;
       if (code.length > MAX_CODE_SIZE) {
         Vue.toasted.error('Source code is too large (' + code.length.toString() + '), limit is ' + MAX_CODE_SIZE.toString(), { duration: 8000 })
@@ -71,11 +87,11 @@ export default {
 
       this.tabIndex = 1;
       this.$refs.output.reset();
+      this.state = 'wait';
 
       const ws = new WebSocket('ws://localhost:1556/run');
 
       ws.addEventListener('open', function () {
-        // TODO: check if the code is too large
         const msg = {
           source_files: [
             {
@@ -90,7 +106,7 @@ export default {
       var msg = null;
       ws.addEventListener('message', (evt) => {
         try {
-          msg = JSON.parse(evt.data) // is it safe?
+          msg = JSON.parse(evt.data)
         } catch (e) {
           Vue.toasted.error('Failed to parse JSON from runjail: ' + e.toString(), { duration: 8000 })
         }
@@ -100,6 +116,9 @@ export default {
           this.$refs.output.addOutput(text, msg.stage);
         }
         if ('event' in msg) {
+          if (msg.event === 'started') {
+            this.state = msg.stage;
+          }
           this.$refs.output.addEvent(msg.event, msg.stage)
         }
         if ('exit_code' in msg) {
@@ -109,9 +128,10 @@ export default {
           this.$refs.output.addDuration(msg.duration_sec, msg.stage)
         }
       });
-      ws.addEventListener('close', function (evt) {
+      ws.addEventListener('close', (evt) => {
+        this.state = 'idle';
       });
-      ws.addEventListener('error', function (evt) {
+      ws.addEventListener('error', (evt) => {
         Vue.toasted.error('Websocket error: ' + JSON.stringify(evt), { duration: 8000 })
       });
     },
@@ -134,5 +154,9 @@ export default {
 
 #run-btn, #menu-btn {
   height: 40px;
+}
+
+#current-state {
+  margin: 0px 10px;
 }
 </style>
